addbfc6dc3d0f60f2f03561c357ad4061771b969
Alex Here <i.alex.here2004@gmail.com>
2025-11-26 18:35:05 +0700

    Squashed commit of the following:
    commit 12ca2c0e6ea4db56618801e40f42233efab1d5ae
    Merge: c03a8b4 e23531f
    Author: Imanuel Alexander Here <85568343+iamalexhere@users.noreply.github.com>
    Date:   Wed Nov 26 18:31:10 2025 +0700
    
        Merge pull request #2 from iamalexhere/feat/sign-in-page
    
        sign-in + sign-up pages
    
    commit e23531fad668938409f49053202d073f341f9c48
    Merge: fcf512c c03a8b4
    Author: Alex Here <i.alex.here2004@gmail.com>
    Date:   Wed Nov 26 18:28:04 2025 +0700
    
        Merge branch 'main' into feat/sign-in-page
    
    commit c03a8b424b469949075f49f5aec66743de0d6d01
    Merge: 1531a3b 44241e9
    Author: Imanuel Alexander Here <85568343+iamalexhere@users.noreply.github.com>
    Date:   Wed Nov 26 18:03:03 2025 +0700
    
        Merge pull request #1 from iamalexhere/feat/catalog-page
    
        Feat/catalog page
    
    commit fcf512c21def910652b8ecc416b1b3ea886ecfd3
    Author: Yohan Leonardo <yohanleonardo150@gmail.com>
    Date:   Wed Nov 26 14:43:59 2025 +0700
    
        sign-in + sign-up pages
    
    commit 44241e90e88d68fd9530b26ac414b45a252f85b9
    Author: zakkib <zzakkibaldani@gmail.com>
    Date:   Wed Nov 26 13:10:29 2025 +0700
    
        Added simple navbar and a hero section including hero image and hero title, and also styled the search bar. Don't stay awake for too long Don't go to bed I'll make a cup of coffee for your head I'll get you up and going out of bed.
    
    commit ea12895701271c017c4356e7155bda0488efac2a
    Author: zakkib <zzakkibaldani@gmail.com>
    Date:   Tue Nov 25 20:44:33 2025 +0700
    
        Making the main component of catalog page
    
        Added the catalog page, including cards using 5 column grid. I also added some mock data to visualize the cards and using online placeholder image from placeholder.co to test the card image. Not yet finish workin on this page. I havent style the page much, including the header and the search bar. Here I am again, writing another piece of code is every line of code that have been written worth it? Wise man said that "life is a waterfall, we are one in the river and one again after the fall". That shi is kinda tuff ngl fr fr no cap on a stack.

---
+++ js/sign-up.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const tosCheckbox = document.getElementById("tos");  
+    const signUpBtn = document.getElementById("signUpBtn");
+
+    const isValidEmail = (email) => {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+    };
+
+    const checkFormValidity = () => {
+        const email = emailInput.value;
+        const password = passwordInput.value;
+        const isTosChecked = tosCheckbox.checked; 
+
+        if (!password || !isValidEmail(email) || !isTosChecked) {
+            signUpBtn.classList.add("disabled-btn");
+            signUpBtn.disabled = true;
+        } else {
+            signUpBtn.classList.remove("disabled-btn");
+            signUpBtn.disabled = false;
+        }
+    };
+
+    emailInput.addEventListener("input", checkFormValidity);
+    passwordInput.addEventListener("input", checkFormValidity);
+    tosCheckbox.addEventListener("change", checkFormValidity); // Listener untuk checkbox
+
+    signUpBtn.addEventListener("click", () => {
+        // alert("Account Created! Redirecting to home");
+            window.location.href = "../html/sign-in.html";
+    });
+});
